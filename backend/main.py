b99384f663dcede1da109f38f7f43b56a676d909
amitgarg31 <as1000amit@gmail.com>
2025-08-06 00:37:08 +0530

    fix

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://voice-bot-chatgpt.onrender.com", "http://localhost:3000"], 
+    allow_origins=[
+    "https://voice-bot-chatgpt.netlify.app",
+    "http://localhost:3000"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
